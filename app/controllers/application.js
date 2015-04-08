7a38af151a44e786133e4f85a62aa5eaba357dd0
horaciof <horacioalexandre.fernandes@gmail.com>
2015-04-08 13:45:48 -0300

    removed unused services; make direct search with client variable

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,26 +1,18 @@
-import Ember from 'ember';
-import {search} from '../utils/elastic';
-var client = new elasticsearch.Client();
+  import Ember from 'ember';
+  var client = new elasticsearch.Client();
 
-export default Ember.Controller.extend({
-  query: "",
-  actions: {
-    search: function () {
-      search(client, this.get('query'), {
-        success: data => {
-          console.log('sucess');
-          console.log(data);
-        },
-        fail: e => {
-          if(e.lengthComputable){
-            console.log(e);
-            // var progress = e.loaded / e.total * 100;
-            // this.set('progressValue', progress);
-          } else {
-            console.log('não tem tamanho. hã ? ');
-          }
-        }
-      });
-    },
-  }
-});
+  export default Ember.Controller.extend({
+    query: "",
+    actions: {
+      search: function () {
+        client.search({
+          q: 'pants'
+        }).then(function (body) {
+          var hits = body.hits.hits;
+          console.log(body);
+        }, function (error) {
+          console.trace(error.message);
+        });
+      }
+    }
+  });
--- config/environment.js
+++ config/environment.js
@@ -2,6 +2,15 @@
 
 module.exports = function(environment) {
   var ENV = {
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' use.typekit.net connect.facebook.net maps.googleapis.com maps.gstatic.com",
+      'font-src': "'self' data: use.typekit.net",
+      'connect-src': "'ws://localhost:35729'",
+      'img-src': "'self' www.facebook.com p.typekit.net",
+      'style-src': "'self' 'unsafe-inline' use.typekit.net",
+      'frame-src': "s-static.ak.facebook.com static.ak.facebook.com www.facebook.com"
+    },
     modulePrefix: 'elastic-ember',
     environment: environment,
     baseURL: '/',
